1750986f99fbbe8c2e9cd1f5226d57ecfc7d372f
Laxer God <laxersan@gmail.com>
2023-05-12 19:45:42 -0500

    Hexagonal Architecture

---
+++ src/test/java/com/ApiRest/ProManagement/ProManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiRest.ProManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
